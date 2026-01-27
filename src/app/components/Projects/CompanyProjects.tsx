24f01a2049e11cf433610fdb81adf8800d0fdc35
Akhil <akhil@mrproptek.com>
2026-01-27 20:53:44 +0530

    changes

--- src/app/components/Projects/CompanyProjects.tsx
+++ src/app/components/Projects/CompanyProjects.tsx
@@ -14,7 +14,7 @@ const companyProjects = [
   {
     name: "Umaid CF",
     url: "https://www.umaidcf.in/",
-    img: "/umaidcf.png",
+    img: "/umaidcf.jpg",
     description:
       "Contributed to the frontend of a community funding platform with focus on UI and usability.",
   },
